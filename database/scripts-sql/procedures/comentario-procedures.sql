dd0b24f0930a865f8c6f712d6edf3dca3cd9ccb1
unknown <edvan.aoliveira324@gmail.com>
2025-12-03 18:39:28 -0300

    feat: add procedure script for getting comments of a log
    also changes post comment procedure name

--- database/scripts-sql/procedures/comentario-procedures.sql
+++ database/scripts-sql/procedures/comentario-procedures.sql
@@ -3,7 +3,7 @@
 -- --------------------------------------------------------
 DELIMITER $$
 -- Criar Comentário
-CREATE PROCEDURE criar_comentario(
+CREATE PROCEDURE publicar_comentario(
 	input_conteudo VARCHAR(255),
     IN input_usuario_id INT,
     IN input_log_id INT
@@ -29,12 +29,39 @@ BEGIN
 	END IF;
 END$$
 
-CALL criar_comentario("Olá mundo!", 1, 31)$$
-SELECT * FROM tbl_log$$
-SELECT * FROM tbl_usuario$$
-SELECT * FROM tbl_comentario$$
-
 -- Puxar comentários de um log
+CREATE PROCEDURE buscar_comentarios_log(
+	IN input_log_id INT
+)
+BEGIN
+	DECLARE log_existe INT;
+    
+    SELECT COUNT(id) FROM tbl_log WHERE id = input_log_id INTO log_existe;
+    IF log_existe = 0
+    THEN
+        SELECT "ERRO_404: O log inserido não foi encontrado na base de dados" message;
+        /*
+        SIGNAL SQLSTATE '45000' 
+        SET MESSAGE_TEXT = 'ERRO_404: O log inserido não foi encontrado na base de dados';
+        */
+    ELSE
+        SELECT 
+		tbl_comentario.usuario_id AS autor_id,
+    
+		tbl_usuario.apelido AS autor_apelido,
+		tbl_usuario.foto_perfil AS autor_foto,
+    
+		tbl_comentario.id AS comentario_id,
+		tbl_comentario.conteudo AS comentario_conteudo,
+		tbl_comentario.data_publicacao
+    
+		FROM tbl_comentario
+		JOIN tbl_usuario ON tbl_usuario.id = tbl_comentario.usuario_id
+		WHERE tbl_comentario.log_id = input_log_id AND tbl_comentario.visivel = 1
+		ORDER BY data_publicacao;
+	END IF;
+END$$ 
+
 -- Deletar um comentário por id
 -- deletar comentários por id de usuário (para desativação)
 
