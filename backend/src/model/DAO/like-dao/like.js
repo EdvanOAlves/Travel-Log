5de785eab81ff401b0e37cb462ce4e364f0ce3a4
GabrielPKTN <gabriellacerda20059@outlook.com>
2025-12-05 18:27:35 -0300

    feat(model): Adiciona camada de persistência de dados da tabela relacionamento curtida do BD

---
+++ backend/src/model/DAO/like-dao/like.js
@@ -0,0 +1,38 @@
+/**********************************************************************
+ * Objetivo: Arquivo da camada de modelagem, responsável pelo CRUD do
+ * realcionamento entre log e usuário que representa curtida.
+ * Data: 05/11/2025
+ * Developer: Gabriel Lacerda Correia
+ * Versão: 1.0.0
+ *********************************************************************/
+
+// Import da dependência do client do prisma para conexão com o BD.
+const { PrismaClient } = require("../../../generated/prisma")
+
+// Criando novo objeto baseado na classe PrismaClient
+const prisma = new PrismaClient()
+
+// Alternna o status de curtido de um log
+const setToggleLikeLog = async (like) => {
+
+    try {
+
+        sql = `CALL AltenarRelacaoLikes(${like.id_log}, ${like.id_usuario})`
+
+        result = await prisma.$executeRawUnsafe(sql)
+
+        if(Array.isArray(result)) {
+            return result
+        } else {
+            return false
+        }
+
+    } catch (error) {
+        return false
+    }
+
+}
+
+module.exports = {
+    setToggleLikeLog
+}
