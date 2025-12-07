4db3184e69adb052ec91d7f34a77340c03baac2e
GabrielPKTN <gabriellacerda20059@outlook.com>
2025-12-07 20:32:25 -0300

    feat(controller): Adiciona camada de validação de dados para o CRUD de curtida

---
+++ backend/src/controller/curtida/controller_curtida.js
@@ -0,0 +1,77 @@
+/**********************************************************************
+ * Objetivo: Arquivo da camada de controle responsável pela validação
+ * de dados da tabela de comentario do BD.
+ * Data: 07/12/2025
+ * Developer: Gabriel Lacerda Correia
+ * Versão: 1.0.0
+ *********************************************************************/
+
+// Importando funções de dependência de dados do usuário
+const curtidaDAO = require("../../model/DAO/like-dao/like.js")
+
+// Importando mensagens de retorno com status code
+const DEFAULT_MESSAGES = require("../module/config_messages.js")
+
+const alternaCurtida = async (curtida, contentType) => {
+
+    MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+        
+    try {
+        
+        if(String(contentType).toUpperCase() == 'APPLICATION/JSON') {
+
+            validar = validarCurtida(curtida)
+            
+            if(!validar) {
+    
+                resultCurtida = await curtidaDAO.setToggleLikeLog(curtida)
+
+                if(resultCurtida) {
+                    MESSAGES.DEFAULT_HEADER.status              = MESSAGES.SUCCESS_UPDATE_ITEM.status
+                    MESSAGES.DEFAULT_HEADER.status_code         = MESSAGES.SUCCESS_UPDATE_ITEM.status_code
+                    MESSAGES.DEFAULT_HEADER.message             = MESSAGES.SUCCESS_UPDATE_ITEM.message
+                    delete MESSAGES.DEFAULT_HEADER.items
+    
+                    return MESSAGES.DEFAULT_HEADER //200
+    
+                } else {
+                    return MESSAGES.ERROR_INTERNAL_SERVER_MODEL //500
+                }
+    
+            } else {
+                return validar //400
+            }
+
+        } else {
+            return MESSAGES.ERROR_CONTENT_TYPE //415
+        }
+    
+    } catch (error) {
+        return MESSAGES.ERROR_INTERNAL_SERVER_CONTROLLER //500
+    }
+
+}
+
+const validarCurtida = (curtida) => {
+
+    MESSAGES = JSON.parse(JSON.stringify(DEFAULT_MESSAGES))
+    
+    if (curtida.log_id == null || curtida.log_id == undefined || curtida.log_id == "" || typeof curtida.log_id !== "number") {
+
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [LOG ID INCORRETO]'
+        return MESSAGES.ERROR_REQUIRED_FIELDS
+
+    } else if (curtida.usuario_id == null || curtida.usuario_id == undefined || curtida.usuario_id == "" || typeof curtida.usuario_id !== "number") {
+
+        MESSAGES.ERROR_REQUIRED_FIELDS.message += ' [USUARIO ID INCORRETO]'
+        return MESSAGES.ERROR_REQUIRED_FIELDS
+
+    } else {
+        return false
+    }
+
+}
+
+module.exports = {
+    alternaCurtida
+}
